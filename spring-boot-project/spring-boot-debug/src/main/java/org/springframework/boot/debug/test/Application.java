a8a1c1a0ffd913765b574916bb1ae42970aded72
kenny <479862661@qq.com>
2023-03-21 13:59:39 +0800

    Initial Commit

---
+++ spring-boot-project/spring-boot-debug/src/main/java/org/springframework/boot/debug/test/Application.java
@@ -0,0 +1,16 @@
+package org.springframework.boot.debug.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kenny
+ * @date 2022-06-22
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
